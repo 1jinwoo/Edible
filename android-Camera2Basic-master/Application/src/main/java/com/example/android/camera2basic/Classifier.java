7bef18072813580f28cc857b44e9421ab00df3b0
Jin Woo Won <jw3580@columbia.edu>
2018-02-18 09:52:03 -0500

    completed classifier.java

--- android-Camera2Basic-master/Application/src/main/java/com/example/android/camera2basic/Classifier.java
+++ android-Camera2Basic-master/Application/src/main/java/com/example/android/camera2basic/Classifier.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Justin Won on 2/18/2018.
+ * @Author Jin Woo Won
+ * @See https://github.com/googlesamples/android-Camera2Basic
+ * This class classifies ingredients to inform the user of the properties of the input food.
  */
-
 public class Classifier {
     private String[] wordsToRemove = {"and", "or", "the", "but", "or", "nor", "for", "yet",
             "so", "prepare", "preparation", "high", "protein", "acid", "products", "half",
@@ -21,6 +22,33 @@ public class Classifier {
             '`', '.', '/', '<', '>', '?', ';', '\'', ':', '"',
             '_', '-', '=', '+', '1', '2', '3', '4', '5', '6',
             '7', '8', '9', '0'};
+    private String[] dairy;
+    private String[] egg;
+    private String[] nuts;
+    private String[] treeNuts;
+    private String[] soy;
+    private String[] wheat;
+    private String[] fish;
+    private String[] meat;
+
+    private String[][][] words = {{dairy}, {egg}, {nuts}, {treeNuts}, {soy}, {wheat}, {fish},
+                                    {meat}, {dairy, egg, fish, meat}, {nuts, treeNuts, dairy,
+                                    soy, wheat, egg, fish}};
+
+
+    /* This constructor reads ingredient files for specific allergies and eating style and
+     * has the pipeline system ready to process input raw data from server.
+     */
+    public Classifier(){
+        dairy = parseList("milkwords.txt");
+        egg = parseList("eggwords.txt");
+        nuts = parseList("peanutwords.txt");
+        treeNuts = parseList("treenutwords.txt");
+        soy = parseList("sorywords.txt");
+        wheat = parseList("wheatwords.txt");
+        fish = parseList("fishwords.txt");
+        meat = parseList("meatwords.txt");
+    }
 
     public String[] webToArray(String rawStr) {
         String[] result = rawStr.split("\n");
@@ -84,6 +112,34 @@ public class Classifier {
         return (String[]) results.toArray();
     }
 
-    
+    /*
+     * @param ingredients, words, testedProperty
+     * Input testedProperty in the form "Nuts", "treeNuts", etc.
+     */
+    public String checkOneProperty(String[] ingredients, String[] words, String testedProperty){
+        String property = "";
+        for (String ingredient : ingredients){
+            for (String word : words){
+                if(ingredient.equals(word)){
+                    property = testedProperty;
+                }
+            }
+        }
+        return "";
+    }
+
+    public String[] checkIngredients(String rawStr){
+        String[] ingredients = webToArray(rawStr);
+        List<String> markedIngredients = new ArrayList<>();
+
+        for( String ingredient : ingredients){
+            for( String[][] arr2D : words){
+                for ( String[] arr : arr2D ){
+                    String property = checkOneProperty(ingredients, arr, "nuts");
+                }
+            }
+        }
+
+    }
 
 }
